2a28a4780fb635d1025ab5b4af9ecdda3974add2
Teddyjay-04 <jamesteddy411@gmail.com>
2025-04-06 21:52:26 +0000

    Final push for the Shelf Manager API project

--- ShelfManager/settings.py
+++ ShelfManager/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookhub',
+    'rest_framework',
+    'rest_framework.authtoken'
+
 ]
 
 MIDDLEWARE = [
@@ -134,3 +137,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+AUTH_USER_MODEL = 'bookhub.CustomUser'
